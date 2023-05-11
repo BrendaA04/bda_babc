309a01a918b441753ea7b67250e41f66c2c380f2
BrendaA04 <122801442+BrendaA04@users.noreply.github.com>
2023-05-11 15:34:25 +0000

    Initial commit

---
+++ main.sql
@@ -0,0 +1,8 @@
+SELECT "Hello world!";
+
+create table personas (
+  id_persona   integer primary key,
+  nombre varchar(30) no null,
+  primer_apellido varchar(30) no null,
+  segundo_apellido  vaechar(30) no null,
+  email varchar(30) no null);
